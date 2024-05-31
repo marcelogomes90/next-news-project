f46fd1fb45801e033ae75d2fbc9958a7c01c3e97
Marcelo <marcelo.sobrinho@outlook.com>
2024-05-31 09:59:35 -0300

    fix: add pagination in news list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,30 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { NewsData } from '@/interfaces/news';
 import { fetchNews } from '@/processes/news';
 import { useQuery } from '@tanstack/react-query';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { Button } from '@/components/ui/button';
 import imageError from '../assets/images/error.png';
 
 export default function Home() {
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [page, setPage] = useState(10);
+
+  const itemsPerPage = useMemo(() => 10, []);
 
   const { data, error, isLoading } = useQuery({
     queryFn: fetchNews,
-    queryKey: ['news', { qtd: itemsPerPage }],
+    queryKey: ['news', { qtd: itemsPerPage, page }],
   });
 
-  const onNewsClick = useCallback((id: number) => () => {
-    // eslint-disable-next-line no-alert
-    alert(`Clicou na notícia com id ${id}`);
+  const onNewsDetailClick = useCallback((item: NewsData) => () => {
+
+  }, []);
+
+  const onNextNewsClick = useCallback(() => {
+    setPage((prev) => prev + 1);
+  }, []);
+
+  const onPreviousNewsClick = useCallback(() => {
+    setPage((prev) => prev - 1);
   }, []);
 
   const renderItem = useCallback((item: NewsData, index: number) => {
@@ -26,10 +36,7 @@ export default function Home() {
 
     return (
       <div key={`NEWS_${item.id}`}>
-        <div
-          className="mb-8 flex justify-between items-center gap-4 cursor-pointer"
-          onClick={onNewsClick(item.id)}
-        >
+        <div className="mb-8 flex justify-between items-center gap-4">
           <img
             src={`https://agenciadenoticias.ibge.gov.br/${image_intro}`}
             alt="imagem da intro"
@@ -40,13 +47,17 @@ export default function Home() {
           <div className="flex-col">
             <h2 className="text-xl font-semibold mb-2">{item.titulo}</h2>
             <p>{item.introducao}</p>
+            <div className="mt-2 flex gap-2 items-center">
+              <span className="text-xs text-muted-foreground">{item.data_publicacao}</span>
+              <Button variant="link" onClick={() => onNewsDetailClick(item)}>Ver matéria completa</Button>
+            </div>
           </div>
         </div>
 
         {index < itemsPerPage - 1 && <Separator className="mb-8" />}
       </div>
     );
-  }, [itemsPerPage, onNewsClick]);
+  }, [itemsPerPage, onNewsDetailClick]);
 
   const renderSkeleton = useCallback((_: any, index: number) => (
     <div key={`SKELETON_${index}`} className="flex gap-4 mb-8 items-center">
@@ -78,7 +89,24 @@ export default function Home() {
         {isLoading ? (
           Array.from({ length: 5 }).map(renderSkeleton)
         ) : (
-          data?.items.map(renderItem)
+          <div>
+            {data?.items.map(renderItem)}
+            <div className="flex justify-center gap-4">
+              <Button
+                disabled={data?.page === 1}
+                onClick={onPreviousNewsClick}
+              >
+                Anterior
+              </Button>
+
+              <Button
+                disabled={data?.page === data?.totalPages}
+                onClick={onNextNewsClick}
+              >
+                Próxima
+              </Button>
+            </div>
+          </div>
         )}
       </div>
     </main>
